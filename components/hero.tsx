11194e685d309abfdb17b2a7e3d9e3e25c0c5852
tony-betamind <p.toan@betamind.co.jp>
2025-04-14 13:01:50 +0700

    adjust hero responsive

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,24 +2,27 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <section className="relative bg-baby-pink/10 h-[calc(100vh-64px)]">
-      <div className="container pr-0 mr-0">
+    <section className="flex md:align-start relative bg-baby-pink/10 h-[calc(100vh-64px)]">
+      <div className="container px-4 md:px-0 md:pr-0 md:mr-0">
         <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
-          <div className="flex flex-col justify-center space-y-4">
+          <div
+            className="flex flex-col justify-center space-y-4 text-center md:text-left"
+            id="hero-left"
+          >
             <div className="space-y-2">
               <h1 className="text-3xl mb-8 font-coiny font-bold text-baby-pink sm:text-5xl xl:text-6xl/none">
                 Chào mừng đến với Odile baby store
               </h1>
-              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400 mx-auto md:mx-0">
                 Cửa hàng mẹ & bé nơi các anh hùng nhỏ tuổi nạp năng lượng với
                 sữa ngon, bỉm êm, và thức ăn dặm bổ dưỡng. Tự hào được đồng
                 hành, chăm sóc và tiếp thêm sức mạnh cho các chiến binh nhí.
               </p>
             </div>
-            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+            <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center md:justify-start">
               <Button
                 size="lg"
-                className="bg-baby-pink hover:bg-baby-pink/90 text-white "
+                className="bg-baby-pink hover:bg-baby-pink/90 text-white"
               >
                 Mua ngay
               </Button>
@@ -34,17 +37,13 @@ export default function Hero() {
           </div>
 
           {/* Image Content - 2 columns */}
-          <div className="relative md:col-span-1 sm:col-span-2 flex items-center justify-center">
+          <div className="hidden md:block relative md:col-span-1 sm:col-span-2 flex items-center justify-center">
             <div className="relative">
               <img
                 src="./hero-img.jpg"
                 alt="Hero image"
-                className="w-full h-full object-cover [clip-path:polygon(20%_0%,_100%_0,_100%_100%,_0%_100%)] h-[calc(100vh-64px)]"
+                className="shadow-xl object-cover [clip-path:polygon(20%_0%,_100%_0,_100%_100%,_0%_100%)] h-[calc(100vh-40px)]"
               />
-
-              {/* Decorative elements */}
-              <div className="absolute -right-4 top-1/4 h-72 w-72 rounded-full bg-baby-pink/10 blur-3xl" />
-              <div className="absolute -left-4 bottom-1/4 h-72 w-72 rounded-full bg-baby-blue/10 blur-3xl" />
             </div>
           </div>
         </div>
